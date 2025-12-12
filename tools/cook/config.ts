9d23534af6c83167ee07ec6203e0d48008f4879b
Younes Jaaidi <younes@marmicode.io>
2025-12-12 15:41:08 +0100

    build: 📦 update exercises

--- tools/cook/config.ts
+++ tools/cook/config.ts
@@ -1,6 +1,31 @@
 import { type Config, type Exercise } from './core.ts';
 
-const exercises: Exercise[] = [];
+const exercises: Exercise[] = [
+  {
+    id: '101-display-recipes',
+    name: 'Display Recipes',
+  },
+  {
+    id: '102-filter-recipes',
+    name: 'Filter Recipes',
+  },
+  {
+    id: '201-recipe-preview',
+    name: 'Recipe Preview',
+  },
+  {
+    id: '202-recipe-filter',
+    name: 'Recipe Filter',
+  },
+  {
+    id: '203-recipe-filter-two-way-binding',
+    name: 'Recipe Filter Two-Way Binding',
+  },
+  {
+    id: '301-recipe-preview-customization',
+    name: 'Recipe Preview Customization',
+  },
+];
 
 export const config: Config = {
   base: 'main',

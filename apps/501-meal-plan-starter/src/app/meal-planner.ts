9a8048294c41b9fa075ca9080c7a8b651ba96503
Younes Jaaidi <younes@marmicode.io>
2025-12-17 21:44:22 +0100

    feat: ✨ enhance 501-meal-plan-starter

---
+++ apps/501-meal-plan-starter/src/app/meal-planner.ts
@@ -0,0 +1,9 @@
+import { Recipe } from './recipe';
+
+export class MealPlanner {
+  addRecipe(recipe: Recipe) {
+    throw new Error('🚧 Work in progress!');
+  }
+}
+
+export const mealPlanner = new MealPlanner();

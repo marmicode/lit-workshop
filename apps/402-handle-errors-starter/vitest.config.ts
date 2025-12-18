1e1191556c28b278c9d36b931dadacbca8f561f5
Younes Jaaidi <younes@marmicode.io>
2025-12-18 22:15:20 +0100

    test: ✅ set up vitest browser mode

---
+++ apps/402-handle-errors-starter/vitest.config.ts
@@ -0,0 +1,31 @@
+import { playwright } from '@vitest/browser-playwright';
+import { defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default defineConfig({
+  ...viteConfig,
+  test: {
+    name: 'whiskmate',
+    watch: false,
+    environment: 'jsdom',
+    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: './test-output/vitest/coverage',
+      provider: 'v8' as const,
+    },
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [
+        {
+          browser: 'chromium',
+        },
+      ],
+      viewport: {
+        width: 1280,
+        height: 720,
+      },
+    },
+  },
+});
